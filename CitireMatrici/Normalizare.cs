9ed42c0935ea962555630f784a4598c3aea90144
cojocaru161 <cojocaru161@gmail.com>
2020-12-04 12:38:00 +0200

    Normalizare Binara

--- CitireMatrici/Normalizare.cs
+++ CitireMatrici/Normalizare.cs
@@ -8,5 +8,23 @@ namespace CitireMatrici
 {
     class Normalizare
     {
+
+        public void NormalizareBinara(Int16[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] > 0)
+                    {
+                        matrix[i, j] = 1;
+                    }
+                }
+            }
+        }
+
     }
 }
