d1c15b6f93bc57bc1fc75c3690efb02f7aa54452
CojocaruGabriel <cojocaru161@gmail.com>
2021-03-19 08:03:56 +0200

    form

--- CitireMatrici/Form1.Designer.cs
+++ CitireMatrici/Form1.Designer.cs
@@ -100,7 +100,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(323, 450);
+            this.ClientSize = new System.Drawing.Size(593, 450);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.btnReadFileTest);
