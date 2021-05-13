b0ffeeb4480461bc822ca7a9464db7a8fa819469
CojocaruGabriel <cojocaru161@gmail.com>
2021-05-13 11:26:39 +0300

    inceput confusionMatrix

--- CitireMatrici/ConfusionMatrix.cs
+++ CitireMatrici/ConfusionMatrix.cs
@@ -9,13 +9,34 @@ namespace CitireMatrici
     public class ConfusionMatrix
     {
         //clasa reala - DA
-        private int TruePositive; //clasa predicitonata DA
-        private int FalseNegative; //clasa predicitonata NU
+        private int TruePositive { get; set; } //clasa predicitonata DA
+        private int FalseNegative { get; set; } //clasa predicitonata NU
 
         //clasa reala - NU
-        private int FalsePositive; //clasa predicitonata DA
-        private int TrueNegative; //clasa predicitonata NU
+        private int FalsePositive{get;set;} //clasa predicitonata DA
+        private int TrueNegative { get; set; } //clasa predicitonata NU
 
+        public ConfusionMatrix(int truePositive, int falseNegative, int falsePositive, int trueNegative)
+        {
+            TruePositive = truePositive;
+            FalseNegative = falseNegative;
+            FalsePositive = falsePositive;
+            TrueNegative = trueNegative;
+        }
 
+        public ConfusionMatrix[] confusionMatrices(List<string> clasaReala, List<string> clasaPredictionata)
+        {
+            ConfusionMatrix[] matrici = new ConfusionMatrix[clasaReala.Count];
+
+            for(var i = 0; i < matrici.Length; i++)
+            {
+                for(var j = 0; j < matrici.Length; j++)
+                {
+
+                }
+            }
+
+            return matrici;
+        }
     }
 }
