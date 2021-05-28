23f5688e2fc81096595a9958624eb558f0deff4b
CojocaruGabriel <cojocaru161@gmail.com>
2021-05-28 22:16:46 +0300

    pachete excel + clasa de scriere excel

---
+++ CitireMatrici/Salvare.cs
@@ -0,0 +1,25 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Drawing.Chart;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitireMatrici
+{
+    public class Salvare
+    {
+        private FileInfo file;
+
+        public void test()
+        {
+            var pack = new ExcelPackage(file);
+            var ws = pack.Workbook.Worksheets.Add("text");
+
+            ws.Cells["A1"].Value = "";
+            ExcelChart chart = ws.Drawings.AddChart("ch", eChartType.ColumnClustered);            
+        }
+    }
+}
